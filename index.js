2a426037577f1bb4b5a60232edbb0cfe56d3de91
Alfonso <alfonso.pru@outlook.com>
2022-11-17 14:51:02 -0700

    commit tree 6df347690d5c238d6d095801cd61835aabfaa46b

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const tempEmployee = { ...employee };
+    tempEmployee[key] = value;
+    return tempEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const tempEmployee = {...employee };
+    delete tempEmployee[key];
+    return tempEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
